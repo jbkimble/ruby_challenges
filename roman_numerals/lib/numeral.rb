83636a2817df7ebf1ba6482fa97e42330d205da5
Jon Kimble <jonathanbkimble@gmail.com>
2017-02-15 22:10:38 -0700

    Refactor convert_to_roman method

--- roman_numerals/lib/numeral.rb
+++ roman_numerals/lib/numeral.rb
@@ -1,23 +1,18 @@
 class Numeral
   def initialize(number)
-    @number = number
-    @remainder = 0
+    @remainder = number
     @final = ""
   end
 
   def convert_to_roman
-    thousands_remainder = convert_place_to_numeral(1000, 'M', @number)
-    nine_hundreds_remainder = convert_place_to_numeral(900, 'CM', thousands_remainder)
-    five_hundreds_remainder = convert_place_to_numeral(500, 'D', nine_hundreds_remainder)
-    four_hundreds_remainder = convert_place_to_numeral(400, 'CD', five_hundreds_remainder)
-    hundreds_remainder = convert_place_to_numeral(100, 'C', four_hundreds_remainder)
-    nineties_remainder = convert_place_to_numeral(90, 'XC', hundreds_remainder)
-    forties_remainder = convert_place_to_numeral(40, 'XL', nineties_remainder)
-    tens_remainder = convert_place_to_numeral(10, 'X', forties_remainder)
-    nines_remainder = convert_place_to_numeral(9, 'IX', tens_remainder)
-    fives_remainder = convert_place_to_numeral(5, 'V', nines_remainder)
-    fours_remainder = convert_place_to_numeral(4, 'IV', fives_remainder)
-    ones = convert_place_to_numeral(1, 'I', fours_remainder)
+    [[1000, 'M'], [900, 'CM'],
+    [500, 'D'], [400, 'CD'],
+    [100, 'C'], [90, 'XC'],
+    [40, 'XL'], [10, 'X'],
+    [9, 'IX'], [5, 'V'],
+    [4, 'IV'], [1, 'I']].each do |num, roman|
+      @remainder = convert_place_to_numeral(num, roman, @remainder)
+    end
     @final
   end
 
